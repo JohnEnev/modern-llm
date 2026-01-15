33087d1fa458539ac5d9f4cdb1b4c44d3db5726d
JohnEnev <enevoldsen.j@gmail.com>
2026-01-15 23:57:16 +0100

    rmsnorm

---
+++ src/model/rmsnorm.py
@@ -0,0 +1,10 @@
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float= 1e-6):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.eps = eps
+
+    def forward(self, x):
+        rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + self.eps)
+        x = x / rms * self.gamma
+        return x
