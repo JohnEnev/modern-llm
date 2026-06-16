6d969aa44516fb8525c83b45e4fd87e48237278a
JohnEnev <enevoldsen.j@gmail.com>
2026-06-17 00:27:06 +0200

    smoke test v2

--- src/model/gpt.py
+++ src/model/gpt.py
@@ -19,8 +19,8 @@ class GPTConfig:
             tie_weights: bool = True,
             use_qk_norm: bool = True,
             use_diff_attn: bool = True,
-            use_mhc: bool = False,
-            n_streams: int = 2,
+            use_mhc: bool = True,
+            n_streams: int = 4,
     ):
         self.vocab_size = vocab_size
         self.d_model = d_model
